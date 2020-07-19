16c12de5a212b2f8f42833256a3b5cb983cde569
bryandonmarc <bryandonmarc@gmail.com>
2020-07-19 03:21:49 +0000

    smol fixes #2

--- Assets/scripts/SceneManagerScript.cs
+++ Assets/scripts/SceneManagerScript.cs
@@ -4,19 +4,11 @@ using UnityEngine.SceneManagement;
 public class SceneManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static SceneManagerScript instance;
-    private void Awake() {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else 
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
+    void Awake()
+    {
+        ServiceLocator.Register<SceneManagerScript>(this);
     }
+    
     public void GameScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
