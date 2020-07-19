803461ce5ca10ecd1511bdb4428172fa4c59b25b
bryandonmarc <bryandonmarc@gmail.com>
2020-07-19 02:34:14 +0000

    Bring back poison cloud animation

--- Assets/scripts/GlassAction.cs
+++ Assets/scripts/GlassAction.cs
@@ -11,6 +11,10 @@ public class GlassAction : MonoBehaviour
     public GameObject[] glassObjects;
     public Queue<Glass> glassesQueue;
 
+    public GameObject poisonCloud;
+    List<GameObject> poisonInstances;
+    List<Vector2> poisonCoordinates;
+
     int _queueLimit = 5;
 
     void Awake()
@@ -20,6 +24,8 @@ public class GlassAction : MonoBehaviour
 
     void Start(){
         glassesQueue = new Queue<Glass>();
+        poisonInstances = new List<GameObject>();
+        poisonCoordinates = new List<Vector2>();
 
         while(glassesQueue.Count < _queueLimit){
             AddGlass();
@@ -37,6 +43,22 @@ public class GlassAction : MonoBehaviour
     public void ReplaceGlass()
     {
         glassesQueue.Dequeue();
+
+        //Destroy all poison gameobject instances in play area
+        if(poisonInstances.Count != 0)
+        {
+            foreach (GameObject p in poisonInstances)
+            {
+                Destroy(p);   
+            }
+            
+
+        }
+
+        //clear poisoncontainer and coordinates everytime player removes glass to reset play area
+        poisonInstances.Clear();
+        poisonCoordinates.Clear();
+
         AddGlass();
         UpdateGlass();
         //DebugGlass();
@@ -46,6 +68,27 @@ public class GlassAction : MonoBehaviour
         foreach ((Glass glass, int i) in glassesQueue.Select((value, i) => (value, i)))
         {
             glassObjects[i].GetComponent<SpriteRenderer>().sprite = _drinks[glass.glassValue % _drinks.Length];
+
+            if(glass.glassValue == 0){
+                GameObject _poison = Instantiate(poisonCloud, glassObjects[i].transform.position + new Vector3(0.1f, 0.3f, 0), Quaternion.identity) as GameObject;
+
+                //checks if there is already poison on play area to prevent multiple instantiation of poison gameobject
+                if(!poisonCoordinates.Contains(_poison.transform.position))
+                {
+                    //add position of instantiated gameobject to coordinates
+                    poisonCoordinates.Add(_poison.transform.position);
+                    //set poison gameobject to parent
+                    _poison.transform.SetParent(glassObjects[i].transform);
+                    //reset rotation of poison as it is the same as parent
+                    _poison.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    //add poison to container
+                    poisonInstances.Add(_poison);
+                }
+                else
+                {
+                    Destroy(_poison);
+                }
+            }
         }
     }
     
